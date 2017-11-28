d9b7ba93fdfa462668d8c4e3c528d384c5adee86
juriasan <giulio.rm95@gmail.com>
2017-11-28 21:15:57 +0300

    CLI added

--- src/main/java/ru/juriasan/Main.java
+++ src/main/java/ru/juriasan/Main.java
@@ -1,15 +1,15 @@
 package ru.juriasan;
 
 import ru.juriasan.services.FileService;
+import ru.juriasan.util.CLI;
 
 import java.io.IOException;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        String dir1 = "C:\\Users\\zotovy\\Documents\\Git\\dirdiff\\src\\main\\resources\\dir1";
-        String dir2 = "C:\\Users\\zotovy\\Documents\\Git\\dirdiff\\src\\main\\resources\\dir2";
-        String dir3 = "C:\\Users\\zotovy\\Documents\\Git\\dirdiff\\src\\main\\resources\\dir3";
-        FileService.getDirectoryManager().diff(dir1, dir2, dir3);
+        CLI cli = new CLI(args);
+        FileService.getDirectoryManager().diff(cli.getFirstDirectory(), cli.getSecondDirectory(),
+                cli.getResultDirectory());
     }
 }
--- src/main/java/ru/juriasan/util/CLI.java
+++ src/main/java/ru/juriasan/util/CLI.java
@@ -1,4 +1,36 @@
 package ru.juriasan.util;
 
 public class CLI {
+
+    private String firstDirectory;
+    private String secondDirectory;
+    private String resultDirectory;
+
+    private static final String USAGE = "" +
+            "Usage:\n\n" +
+            "dirdiff dir1 dir2 dir3, where dir1, dir2 - paths to directories which will be compared, " +
+            "dir3 - path to resulting directory.";
+
+    public CLI(String...args) {
+        if (args.length != 3) {
+            System.out.println(USAGE);
+            throw new RuntimeException();
+        }
+
+        this.firstDirectory = args[0];
+        this.secondDirectory = args[1];
+        this.resultDirectory = args[2];
+    }
+
+    public String getFirstDirectory() {
+        return this.firstDirectory;
+    }
+
+    public String getSecondDirectory() {
+        return this.secondDirectory;
+    }
+
+    public String getResultDirectory() {
+        return this.resultDirectory;
+    }
 }
