0eeb50393ac0463e6f77e8e04d372c0d11e6f33c
juriasan <giulio.rm95@gmail.com>
2017-11-28 13:07:57 +0300

    added initial functionality for load balancing

--- src/main/java/ru/juriasan/services/FileService.java
+++ src/main/java/ru/juriasan/services/FileService.java
@@ -27,6 +27,7 @@ public abstract  class FileService {
 
     private volatile static DirectoryService directoryManager;
     private volatile static PlainFileService plainFileManager;
+    protected LoadBalancer loadBalancer = LoadBalancer.getInstance(LoadBalancer.MAXIMUM_POOL_SIZE);
 
     protected LoadBalancer loadBalancer = LoadBalancer.getInstance(LoadBalancer.MAXIMUM_POOL_SIZE);
 
